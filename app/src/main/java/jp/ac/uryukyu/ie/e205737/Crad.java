44821cad044c4eb1ab8f0c13de11dd07e9d9494f
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-02-18 19:01:47 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205737/Crad.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205737/Crad.java
@@ -1,5 +1,23 @@
 package jp.ac.uryukyu.ie.e205737;
 
+/**
+ * トランプのカード全体を表すクラス
+ */
 public class Crad {
     
+    /**ジョーカーを表す定数 */
+    private static final int JOKER = 0;
+
+    /**スペードを表す定数 */
+    private static final int SUIT_SPADE = 1;
+
+    /**ダイヤを表す定数 */
+    private static final int SUIT_DIAMOND = 2;
+
+    /**クラブを表す定数 */
+    private static final int SUIT_CLUB = 3;
+
+    /**ハートを表す定数 */
+    private static final int SUIT_HEART = 4;
+
 }
