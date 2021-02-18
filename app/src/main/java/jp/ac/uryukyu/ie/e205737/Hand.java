df2a065e7138f0d63e10f6e6806b2d4559abb7b9
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-02-18 22:59:26 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205737/Hand.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205737/Hand.java
@@ -1,5 +1,47 @@
 package jp.ac.uryukyu.ie.e205737;
 
+import java.util.ArrayList;
+
+
+/**
+ * ババ抜きの手札を表すクラス
+ */
 public class Hand {
     
+    /**手札のカードを入れるリスト
+     * 
+     */
+    private ArrayList hand_ = new ArrayList();
+
+
+    /**
+     * カードを手札に加えるメソッド
+     * @param _card　加えるカード
+     */
+    public void addCard(Card _card){
+         
+        //カードをリストの一番後ろに追加
+        hand_.add(_card);
+    }
+
+    /**
+     * カードを引くメソッド
+     * @return 引いたカード
+     */
+    public Card pickCard(){
+        Card pickedCard = (Card) hand_.remove(0);
+
+        return pickedCard;
+    }
+
+    /**
+     * 手持ちのカードをシャッフルするメソッド
+     */
+    public void shuffle(){
+
+    }
+
+    public int getNumverOfHandCards(){
+        return hand_.size();
+    }
 }
