16c1935814adb98cacb21e48161719f14bce2e7f
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-02-18 19:59:18 +0900

    3rd commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e205737/Card.java
@@ -0,0 +1,51 @@
+package jp.ac.uryukyu.ie.e205737;
+
+/**
+ * トランプのカード全体を表すクラス
+ */
+public class Card {
+    
+    /**ジョーカーを表す定数 */
+    private static final int JOKER = 0;
+
+    /**スペードを表す定数 */
+    private static final int SUIT_SPADE = 1;
+
+    /**ダイヤを表す定数 */
+    private static final int SUIT_DIAMOND = 2;
+
+    /**クラブを表す定数 */
+    private static final int SUIT_CLUB = 3;
+
+    /**ハートを表す定数 */
+    private static final int SUIT_HEART = 4;
+
+    /**
+     * カードの示すマーク
+     */
+    private int suit;
+
+    /**
+     * カードの示す数字
+     */
+    private int number;
+
+    /**
+     * 引数が、マークを示す数字とカードの数字であるコンストラクタ
+     * 
+     * @param _suit　カードのマーク
+     * @param _number　カードの数字
+     */
+    public Card(int _suit, int _number){
+        
+        this.suit = _suit;
+        this.number = _number;
+    }
+
+
+    public int getNumber(){
+        return number;
+    }
+    
+
+}
