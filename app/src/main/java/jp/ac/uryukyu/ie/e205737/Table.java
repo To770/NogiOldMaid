37ec5b1441049831bf6409c309999d5724c161cb
Tomo AKAMINE <e205737@ie.u-ryukyu.ac.jp>
2021-02-19 15:08:10 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205737/Table.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205737/Table.java
@@ -1,5 +1,19 @@
 package jp.ac.uryukyu.ie.e205737;
 
+import java.util.ArrayList;
+
+/**
+ * 場を表すクラス
+ */
 public class Table {
+    private ArrayList discards = new ArrayList();
+    
+    /**
+     * カードを捨てるメソッド
+     * @param 捨てるカードの配列
+     */
+    public void discards(Card card[]){
+
+    }
     
 }
